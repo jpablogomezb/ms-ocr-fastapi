c5e612e5a6c63191e930cd96480d75cc019d3ffb
José P. Gómez B. S <jpablo.gomezb@hotmail.com>
2022-08-05 00:14:23 +0200

    ocr endpoint with pytesseract

--- app/main.py
+++ app/main.py
@@ -1,19 +1,74 @@
+import io
 import pathlib
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+import uuid
+import pytesseract
+from functools import lru_cache
+from fastapi import (
+    FastAPI,
+    HTTPException,
+    Depends,
+    Request,
+    File,
+    UploadFile,
+)
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseSettings
+from PIL import Image
+
+
+class Settings(BaseSettings):
+    debug: bool = False
+    echo_active: bool = False
+
+    class Config:
+        env_file = ".env"
+
+
+@lru_cache()
+def get_settings():
+    return Settings()
+
+
+settings = get_settings()
+DEBUG = settings.debug
 
 BASE_DIR = pathlib.Path(__file__).parent
+UPLOADS_DIR = BASE_DIR / "uploads"
 app = FastAPI()
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
 
 
 @app.get("/", response_class=HTMLResponse)
-def home_view(request: Request):
+def home_view(request: Request, settings: Settings = Depends(get_settings)):
     return templates.TemplateResponse("home.html", {"request": request, "data": 123})
 
 
-
 @app.post("/")
-def home_detail_view():
-    return {"api": "v1"}
+async def ocr_prediction_view(file: UploadFile = File(...)):
+    bytes_str = io.BytesIO(await file.read())
+    try:
+        img = Image.open(bytes_str)
+    except:
+        raise HTTPException(detail="Invalid image", status_code=400)
+    preds = pytesseract.image_to_string(img)
+    predictions = [x for x in preds.split("\n")]
+    return {"results": predictions, "original": preds}
+
+
+@app.post("/img-view/", response_class=FileResponse)
+async def img_view(file: UploadFile = File(...),
+                   settings: Settings = Depends(get_settings)):
+    if not settings.echo_active:
+        raise HTTPException(detail="Invalid endpoint", status_code=400)
+    UPLOADS_DIR.mkdir(exist_ok=True)
+    bytes_str = io.BytesIO(await file.read())
+    try:
+        img = Image.open(bytes_str)
+    except:
+        raise HTTPException(detail="Invalid image", status_code=400)
+    f_name = pathlib.Path(file.filename)
+    f_ext = f_name.suffix
+    f_dest = UPLOADS_DIR / f"{uuid.uuid1()}{f_ext}"
+    img.save(f_dest)
+    return f_dest
--- app/test_endpoints.py
+++ app/test_endpoints.py
@@ -1,5 +1,9 @@
+import io
+import shutil
+import time
 from fastapi.testclient import TestClient
-from app.main import app
+from app.main import app, BASE_DIR, UPLOADS_DIR
+from PIL import Image, ImageChops
 
 client = TestClient(app)
 
@@ -11,8 +15,45 @@ def test_get_home():
     assert "text/html" in response.headers['content-type']
 
 
-def test_post_home():
+def test_invalid_file_upload_error():
     response = client.post("/")
-    assert response.status_code == 200
+    assert response.status_code == 422
     assert "application/json" in response.headers['content-type']
-    # assert response.json() == {"api": "v1"}
+
+
+def test_prediction_upload():
+    img_saved_path = BASE_DIR / "images"
+    for path in img_saved_path.glob("*"):
+        try:
+            img = Image.open(path)
+        except:
+            img = None
+        response = client.post("/", files={"file": open(path, 'rb')})
+        if img is None:
+            assert response.status_code == 400
+        else:
+            # returning a valid image
+            assert response.status_code == 200
+            data = response.json()
+            assert len(data.keys()) == 2
+
+
+def test_view_upload():
+    img_saved_path = BASE_DIR / "images"
+    for path in img_saved_path.glob("*"):
+        try:
+            img = Image.open(path)
+        except:
+            img = None
+        response = client.post("/img-view/", files={"file": open(path, 'rb')})
+        if img is None:
+            assert response.status_code == 400
+        else:
+            # returning a valid image
+            assert response.status_code == 200
+            r_stream = io.BytesIO(response.content)
+            echo_img = Image.open(r_stream)
+            difference = ImageChops.difference(echo_img, img).getbbox()
+            assert difference is None
+    # time.sleep(3)
+    shutil.rmtree(UPLOADS_DIR)
